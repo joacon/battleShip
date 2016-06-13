57c2a87754342135c95f602661a912bbf30bd629
joacon <joaquin.nacht@ing.austral.edu.ar>
2016-06-13 17:45:05 -0300

    messages

--- public/javascripts/websocket.js
+++ public/javascripts/websocket.js
@@ -2,7 +2,6 @@ window.onload = init;
 var socket = new WebSocket("ws://localhost:9000/socket");
 socket.onmessage = onMessage;
 
-
 function post(message) {
     console.log(message);
     socket.send(message);
@@ -10,6 +9,7 @@ function post(message) {
 
 function onMessage(event) {
     console.log(event);
+    // var json = JSON.parse(event.data);
     if(event.data === "Layout"){
         setTimeout(function () {
             $('.game').css("display", "block");
@@ -19,9 +19,25 @@ function onMessage(event) {
     }else if(event.data === "Opponent left"){
         $('.game').css("display", "none");
         $('.opponent-left').css("display", "block");
-    }else if(event.data === "Play"){
-        alert("start game");
+    }else if(event.data === "Play"){ //este jugador puede jugar
+        alert("This player can fire");
+    }else if(json.action === "Fire"){//cuando te llega esto tenes que ver si te pego a algun barco
+        alert("fire");
+    }else if(event.data === "Hit"){ //Su ultimo tiro pego en el barco del otro
+        alert("Hit");
+    }else if(event.data === "Miss"){// Su ultimo tiro pifio el barco del otro
+        alert("Miss");
+    }else if(event.data === "Wait"){//este jugador esta esperando a que el otro juegue
+        alert("Waiting -> the other player is firing");
     }
+}
+
+function fire(x, y) {//pegale a este metodo para disparar
+    post("fire"+"-"+x+"-"+y);
+}
+
+function sendFeedback(hit) {//hit es true si pego y false si no pego
+    post("feedback-" + hit);
 }
 
 
