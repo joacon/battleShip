bdc9e271ee53659283e03a673ed9b9067fdc04ab
Francisco Di Giandomenico <francisco.di@ing.austral.edu.ar>
2016-06-13 17:48:19 -0300

    changed from generating html in javascript to just having the buttons in the html. tried to make it prettier too. Ready and Unready button alternate in play

--- public/javascripts/game-buttons.js
+++ public/javascripts/game-buttons.js
@@ -1,13 +1,43 @@
 /**
  * Created by yankee on 11/06/16.
  */
+
+var readyButton = "<div><button class='btn-success' id='ready-btn'>Ready</button></div>";
+var unreadyButton = "<div><button class='btn-danger' id='unready-btn'>Unready</button></div>";
 ($(function () {
     var buttonPanel = $("#button-panel");
     var boatsPanel = $("#boats");
-    buttonPanel.append("<button class='btn-success' id='ready-btn'>Ready</button>");
-    buttonPanel.append("<button class='btn-danger' id='unready-btn'>Unready</button>");
-    for (var i = 0; i < 4; i++) {
-        boatsPanel.append("<button class='btn-default' id='boat-selector-'" + i + "></button>");
-    }
+    /*buttonPanel.append("<button class='btn-success' id='ready-btn'>Ready</button>");
+     buttonPanel.append("<button class='btn-danger' id='unready-btn'>Unready</button>");*/
+    /*    for (var i = 0; i < 4; i++) {
+     boatsPanel.append("<button class='btn-default boat-btn' id='boat-selector-'" + i + "></button>");
+     }*/
 
+    $("#ready-btn").click(function () {
+        ready();
+        console.log("Ready!");
+    });
+    $("#unready-btn").click(function () {
+        unready();
+    });
 }));
+
+var ready = function () {
+    //$("#ready-btn").css("visibility", "hidden");
+    //$("#unready-btn").css("visibility", "visible");
+    $("#ready-btn").remove();
+    $("#button-panel").prepend(unreadyButton);
+    $("#unready-btn").click(function () {
+        unready();
+    });
+};
+
+var unready = function () {
+    //$("#ready-btn").css("visibility", "visible");
+    //$("#unready-btn").css("visibility", "hidden");
+    $("#unready-btn").remove();
+    $("#button-panel").prepend(readyButton);
+    $("#ready-btn").click(function () {
+        ready();
+    });
+};
--- public/javascripts/websocket.js
+++ public/javascripts/websocket.js
@@ -10,16 +10,16 @@ function post(message) {
 
 function onMessage(event) {
     console.log(event);
-    if(event.data === "Layout"){
+    if (event.data === "Layout") {
         setTimeout(function () {
             $('.game').css("display", "block");
             $('.waiting').css("display", "none");
-        },1500);
+        }, 1500);
         $('.waiting-text').text("Matching with oponent");
-    }else if(event.data === "Opponent left"){
+    } else if (event.data === "Opponent left") {
         $('.game').css("display", "none");
         $('.opponent-left').css("display", "block");
-    }else if(event.data === "Play"){
+    } else if (event.data === "Play") {
         alert("start game");
     }
 }
